22da912242a0f5cb4a82ae289f299af2d87f3964
lijeongwu <user32213459@DESKTOP-D2JJTMD>
2025-05-14 01:39:29 +0900

    test.py 기능 추가

--- test.py
+++ test.py
@@ -1,31 +1,37 @@
 import requests
 import folium
 import webview
+import random
 from geopy.geocoders import Nominatim
 
 #도커 서버 실행 명령어
 #docker run -t -i -p 5000:5000 -v "${PWD}/osrm_data:/data" osrm/osrm-backend osrm-routed --algorithm mld /data/south-korea-latest.osrm
 
-
-
-# 출발지를 입력받는 기능 ? 출발지를 geopy를 통해 좌표값으로 변환하고 출발지만 거기로 설정
 # start와 end만 일정 범위 내의 난수를 생성하도록 만들면 일정 범위 내의 좌표만 만들도록 가능 - 간단하게 가능
 # 목적지 매핑은 난수를 생성한 후 난수값을 확인해서 범위 내의 난수는 특정 지역의 좌표 or 역으로 출발지를 지정하도록 수정
 # - 여기서는 카카오 로컬 API 사용이 필요할듯?
 # 출발지와 목적지 좌표 (예: 제주도 일부)
 
 # 입력받은 주소값을 좌표로 변환해서 start에 할당
+address = '죽전캠퍼스 경기도 용인시 수지구 죽전로 152'
+geoloc = Nominatim(user_agent = 'Random_Trip', timeout=None)
+geo = geoloc.geocode(address)
+lat = str(geo.latitude)
+log = str(geo.longitude)
+
+
 
 # end는 난수 생성기를 통해 일정 범위 내의 목적지만 만들기
-
 ## 추후 카카오 로컬 API 를 통해 나온 난수 값의 범위 내의 역으로 목적지를 지정
 
+start = (lat, log)  # 테스트용 위도, 경도
+end = (33.3842, 126.3323) # 제주도
+# end = (34.1433, 126.8543) # 청산도
+# end = (33.0000, 126.5000) # 바다 한가운데
 
-start = (33.3840, 126.3731)  # 테스트용 위도, 경도
-end = (33.3842, 126.3323)
 
 # OSRM API 요청
-url = f"http://localhost:5000/route/v1/driving/{start[1]},{start[0]};{end[1]},{end[0]}?overview=full&geometries=geojson"
+url = f"http://localhost:5000/route/v1/driving/{start[1]},{start[0]};{end[1]},{end[0]}?overview=full&geometries=geojson&steps=true"
 response = requests.get(url)
 
 if response.status_code != 200:
